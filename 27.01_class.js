56e8d6aeff9017fa24ea354123163afc328982ba
Алекса <aleksa@MacBook-Pro-Aleksa.local>
2023-01-31 13:32:29 -0600

    add dresses store

---
+++ 27.01_class.js
@@ -0,0 +1,109 @@
+let dress1 = {
+title:"Ocean",
+price:"500$",
+image:"./images/blueLongDress.jpeg",
+};
+
+console.log(dress1.title);
+
+let card1Title=document.querySelector("#card1-title");
+
+let card1Image=document.querySelector("#card1-image");
+
+let card1Price=document.querySelector("#card1-price");
+
+card1Title.innerHTML=dress1.title;
+card1Price.innerHTML=dress1.price;
+card1Image.src=dress1.image;
+
+let dress2 = {
+    title:"Lilly",
+    price:"100$",
+    image:"./images/lillyDress.jpeg",
+    };
+    
+    console.log(dress2.title);
+    
+    let card2Title=document.querySelector("#card2-title");
+    
+    let card2Image=document.querySelector("#card2-image");
+    
+    let card2Price=document.querySelector("#card2-price");
+    
+    card2Title.innerHTML=dress2.title;
+    card2Price.innerHTML=dress2.price;
+    card2Image.src=dress2.image;
+
+
+    let dress3 = {
+        title:"Princess",
+        price:"200$",
+        image:"./images/blueDress.jpeg",
+        };
+        
+        console.log(dress3.title);
+        
+        let card3Title=document.querySelector("#card3-title");
+        
+        let card3Image=document.querySelector("#card3-image");
+        
+        let card3Price=document.querySelector("#card3-price");
+        
+        card3Title.innerHTML=dress3.title;
+        card3Price.innerHTML=dress3.price;
+        card3Image.src=dress3.image;
+        
+    let dress4 = {
+            title:"Lake",
+            price:"150$",
+            image:"./images/pinkDress.jpeg",
+            };
+            
+            console.log(dress4.title);
+            
+            let card4Title=document.querySelector("#card4-title");
+            
+            let card4Image=document.querySelector("#card4-image");
+            
+            let card4Price=document.querySelector("#card4-price");
+            
+            card4Title.innerHTML=dress4.title;
+            card4Price.innerHTML=dress4.price;
+            card4Image.src=dress4.image;
+
+    let dress5 = {
+                title:"Flor",
+                price:"550$",
+                image:"./images/purpleLongDress.jpeg",
+                };
+                
+                console.log(dress5.title);
+                
+                let card5Title=document.querySelector("#card5-title");
+                
+                let card5Image=document.querySelector("#card5-image");
+                
+                let card5Price=document.querySelector("#card5-price");
+                
+                card5Title.innerHTML=dress5.title;
+                card5Price.innerHTML=dress5.price;
+                card5Image.src=dress5.image;
+
+    let dress6 = {
+                    title:"Rosa",
+                    price:"550$",
+                    image:"./images/redLongDress.jpeg",
+                    };
+                    
+                    console.log(dress6.title);
+                    
+                    let card6Title=document.querySelector("#card6-title");
+                    
+                    let card6Image=document.querySelector("#card6-image");
+                    
+                    let card6Price=document.querySelector("#card6-price");
+                    
+                    card6Title.innerHTML=dress6.title;
+                    card6Price.innerHTML=dress6.price;
+                    card6Image.src=dress6.image;
+                       
